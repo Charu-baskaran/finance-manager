d2abad8e21727ab880d5b63f3f442bcc3e5966b0
Charu <149860352+Charu-baskaran@users.noreply.github.com>
2025-03-02 15:35:13 +0530

    Update Database.js

--- Backend/DB/Database.js
+++ Backend/DB/Database.js
@@ -1,23 +1,3 @@
-// import mongoose from 'mongoose';
-// import dotenv from 'dotenv';
-
-// dotenv.config();
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGO_URI, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log('MongoDB Connected...');
-//   } catch (error) {
-//     console.error('MongoDB Connection Error:', error);
-//     process.exit(1);
-//   }
-// };
-
-// export default connectDB; 
-
 import mongoose from "mongoose";
 
 export const connectDB = async (req, res) => {
